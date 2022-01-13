5586e041f63e4f545671781838cf3a3c77208af1
Eugen-Ko <kovalchuk.eak@gmail.com>
2022-01-13 17:14:45 +0200

    pages toggle
    Хедере все таки прописал паддинги - иначе гуляет 6пх за счет того что филм свг на 6пх меньше чем высота Filmoteka. А когда на 320пх фильмотека пропадает - сдвиг на 6пх. Рефы вынес. хедер хтмл снял комменты и добавил ид на хедер.

---
+++ src/js/components/refs.js
@@ -0,0 +1,15 @@
+export const refs = {
+  homeButton: document.querySelector('[data-page="home"]'),
+  libraryButton: document.querySelector('[data-page="library"]'),
+  header: document.querySelector('.header'),
+  activeLink: document.querySelector('.nav__list'),
+  inputValue: document.querySelector('#js-input'),
+  search: document.querySelector('#js-search'),
+  libraryBtns: document.querySelector('#js-buttons'),
+  watchedBtn: document.querySelector('.js-library-watched'),
+  queueBtn: document.querySelector('.js-library-queue'),
+
+  filmsList: document.querySelector('.films__container'),
+  
+
+};
