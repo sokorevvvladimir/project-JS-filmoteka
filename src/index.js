e19b1ec12d9ddf159301e76d257a33439b16d91d
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2022-01-14 23:02:50 +0200

    Modal work in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
 
-import './js/components/togglePages'
+import './js/components/togglePages';
+import './js/components/film-description-modal';
--- src/js/components/film-description-modal.js
+++ src/js/components/film-description-modal.js
@@ -1,1 +1,14 @@
+"use strict";
 
+const modalDialog = document.querySelector(".modal-one-film");
+const closeButton = document.querySelector(".modal-close-btn");
+
+closeButton.addEventListener('click', () => {
+    modalDialog.classList.toggle('modal-one-film--hidden')
+});
+
+document.querySelectorAll('.films__card').forEach(card => {
+    card.addEventListener('click', () => {
+        modalDialog.classList.toggle('modal-one-film--hidden')
+    });
+});
