56921ba85ac9876b06f958694b1087cf266f25b5
forysiak-vova <volodja.forus@gmail.com>
2022-01-16 00:45:32 +0100

    запрос

--- src/js/components/createMoviesList.js
+++ src/js/components/createMoviesList.js
@@ -3,23 +3,24 @@
 
 import { refs } from '../utils/refs.js';
 import movieCard from '../templates/movieCard.hbs';
-
+import MovieApiService from '../api/fetch-api.js';
 //-------------------------------------------
 // временный код. Вызывается первая страница списка популярных фильмов
 // что бы сделать разметку и цсс под карточку
 // потом его удалить
 import { normalizationMovieObj } from '../utils/normalizationObj';
-import settings from '../utils/settings';
-const { BASE_URL, API_KEY } = settings;
+// import settings from '../utils/settings';
+// const { BASE_URL, API_KEY } = settings;
 
-async function fetchPopular() {
-  const urlPopular = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=3`;
-  return fetch(urlPopular).then(response => response.json());
-}
+// async function fetchPopular() {
+//   const urlPopular = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=1`;
+//   return fetch(urlPopular).then(response => response.json());
+// }
+const movieApiService = new MovieApiService();
 
 export async function PopularMovies() {
   try {
-    const moviesList = await fetchPopular();
+    const moviesList = await movieApiService.fetchPopular();
     const { results } = moviesList;
     renderMoviesList(results);
   } catch (error) {}
@@ -31,11 +32,14 @@ PopularMovies();
 // results - это объект на входе
 
 export function renderMoviesList(results) {
+  console.log(results)
   // нормализация объекта с учетом обрезания жанров до 2+other...
   const normalObjs = results.map(element => {
     let temp = normalizationMovieObj(element);
+    console.log(element)
     // Обрезание колличества жанров
-    temp.genre.length <= 3
+    temp.genre.length <= 2
+      console.log( temp.genre)
       ? (temp.genre = temp.genre.join(', '))
       : (temp.genre = [temp.genre[0], temp.genre[1], 'Other'].join(', '));
     // Обрезание длины названия фильма
