7975ca60da2a90656f559847b38dece7fc7935a5
Eugen Kovalchuk <kovalchuk.eak@gmail.com>
2022-01-15 09:01:46 +0200

    shot genres and titles
    изменил место проведения укорачивания, поскольку нормализешрОбж - может использоваться как для листа так и для модалки - а в модалке укорачивания не нужно.

--- src/js/components/createMoviesList.js
+++ src/js/components/createMoviesList.js
@@ -1,19 +1,19 @@
 // Сюда передавать массив объектов с фильмами
 // и здесь генерится разметка карточек фильмов
 
-import {refs} from '../utils/refs.js';
-import movieCard from '../timplates/movieCard.hbs'
+import { refs } from '../utils/refs.js';
+import movieCard from '../timplates/movieCard.hbs';
 
 //-------------------------------------------
 // временный код. Вызывается первая страница списка популярных фильмов
 // что бы сделать разметку и цсс под карточку
 // потом его удалить
-import {normalizationMovieObj} from '../utils/normalizationObj';
+import { normalizationMovieObj } from '../utils/normalizationObj';
 import settings from '../utils/settings';
-const {BASE_URL, API_KEY} = settings;
+const { BASE_URL, API_KEY } = settings;
 
 async function fetchPopular() {
-  const urlPopular = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=1`;
+  const urlPopular = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=3`;
   return fetch(urlPopular).then(response => response.json());
 }
 
@@ -23,9 +23,8 @@ export async function PopularMovies() {
     const { results } = moviesList;
     console.log(moviesList);
     renderMoviesList(results);
-  }
-  catch (error) {
-    console.log(error);  
+  } catch (error) {
+    console.log(error);
   }
 }
 
@@ -35,7 +34,18 @@ PopularMovies();
 // results - это объект на входе
 
 export function renderMoviesList(results) {
-  const normalObjs = results.map(element => normalizationMovieObj(element));
+  // нормализация объекта с учетом обрезания жанров до 2+other...
+  const normalObjs = results.map(element => {
+    let temp = normalizationMovieObj(element);
+    // Обрезание колличества жанров
+    temp.genre.length <= 3
+      ? (temp.genre = temp.genre.join(', '))
+      : (temp.genre = [temp.genre[0], temp.genre[1], 'other...'].join(', '));
+    // Обрезание длины названия фильма
+    if (temp.title.length > 37) temp.title = temp.title.slice(0, 37) + '...';
+    return temp;
+  });
   console.log(normalObjs); // Потом удалить
+
   refs.filmsList.insertAdjacentHTML('beforeend', movieCard(normalObjs));
 }
--- src/js/utils/normalizationObj.js
+++ src/js/utils/normalizationObj.js
@@ -1,26 +1,23 @@
-// Функция на входе получает объект с фильмотеки и преобразует его 
+// Функция на входе получает объект с фильмотеки и преобразует его
 // в нормальный вид. Поскольку в фильмотеке объект фильм имеет жанры в виде массива кодов
 // возможно отсутствие картинок и кривость вида даты.
 
 import genreList from './genreList';
 
-import settings from "./settings";
-const {API_KEY, IMG_URL} = settings;
+import settings from './settings';
+const { API_KEY, IMG_URL } = settings;
 
 import noImg from '../../images/noImageAvailable.jpg';
 
 // -------------------------------------------------------------
-const getGenreArray = (genre_ids) =>
-  (genre_ids.length <= 3) ? genre_ids.map(el => genreList[`${el}`]).join(', ') :
-  [genreList[`${genre_ids[0]}`], genreList[`${genre_ids[1]}`], 'other...'].join(', ');
+const getGenreArray = genre_ids => genre_ids.map(el => genreList[`${el}`]);
 
-const getImgPath = (imgPath) => (!imgPath) ?  `${noImg}` : `${IMG_URL}${imgPath}`;
+const getImgPath = imgPath => (!imgPath ? `${noImg}` : `${IMG_URL}${imgPath}`);
 
-const getDate = (date) => (!date) ?  data : date.slice(0, 4);
+const getDate = date => (!date ? data : date.slice(0, 4));
 
 // -------------------------------------------------------------
-export const normalizationMovieObj = 
-({
+export const normalizationMovieObj = ({
   genre_ids,
   id,
   original_title,
@@ -29,7 +26,7 @@ export const normalizationMovieObj =
   poster_path,
   release_date,
   vote_average,
-  vote_count,  
+  vote_count,
 }) => ({
   genre: getGenreArray(genre_ids),
   id: id,
@@ -39,5 +36,5 @@ export const normalizationMovieObj =
   img: getImgPath(poster_path),
   releaseDate: getDate(release_date),
   vote: vote_average,
-  votes: vote_count,  
+  votes: vote_count,
 });
