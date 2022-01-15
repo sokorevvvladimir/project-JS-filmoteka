45e91c05e8dd340448cdbae4c72664adff92ce13
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2022-01-15 23:44:05 +0200

    Modal

--- src/js/components/film-description-modal.js
+++ src/js/components/film-description-modal.js
@@ -7,8 +7,13 @@ closeButton.addEventListener('click', () => {
     modalDialog.classList.toggle('modal-one-film--hidden')
 });
 
-document.querySelectorAll('.films__card').forEach(card => {
-    card.addEventListener('click', () => {
-        modalDialog.classList.toggle('modal-one-film--hidden')
-    });
+document.querySelector('.films__container').addEventListener('click', (event) => {
+    for (let el of event.path) {
+        // идем наверх по иерархии элементов, так как может быть, что кликнули на внутренний span
+        if ( el.classList?.contains('films__card')) { 
+            // если среди классов есть карточка фильма, показываем модалку 
+            modalDialog.classList.toggle('modal-one-film--hidden');
+            return;
+        }
+    }
 });
