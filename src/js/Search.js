0f0e7d4da3cedcd754fbe26d65761a4d0c1bff93
VladimirBludov <vladimir.bloodov@gmail.com>
2022-01-15 11:04:17 +0200

    fix-project-settings

--- src/js/Search.js
+++ src/js/Search.js
@@ -1,35 +1,37 @@
-import MovieApiService from './api/fetch-api.js'
+import MovieApiService from './api/fetch-api.js';
 import { refs } from './utils/refs.js';
 import Notiflix from 'notiflix';
-// import renderImages from '../tamplates/film-cards.hbs';
 
-const { serchKeyword, filmsList} = refs;
+const { searchKeyword, filmsList } = refs;
 
 const movieApiService = new MovieApiService();
-serchKeyword.addEventListener('submit', onSearch);
+searchKeyword.addEventListener('submit', onSearch);
 
 function onSearch(e) {
   e.preventDefault();
 
-  let searchQuery =e.target[0].value;  //e.currentTarget.elements.searchQuery.value.trim();
- movieApiService.query = searchQuery;
+  let searchQuery = e.target[0].value; //e.currentTarget.elements.searchQuery.value.trim();
+  movieApiService.query = searchQuery;
 
-  if (movieApiService.query.length === 0 ) {
-   return Notiflix.Notify.info('Sorry, there are no video matching your search query. Please try again.');
-   
-  } 
- fetchMoviesBySearch()
+  if (movieApiService.query.length === 0) {
+    return Notiflix.Notify.info(
+      'Sorry, there are no video matching your search query. Please try again.',
+    );
+  }
+  fetchMoviesBySearch();
 }
 
 function fetchMoviesBySearch() {
   //идет запрос на fetch
-  movieApiService.fetchMoviesBySearch().then(object => {
-  appendHitsMarkup(object)
-  }).catch(console.log);
+  movieApiService
+    .fetchMoviesBySearch()
+    .then(object => {
+      appendHitsMarkup(object);
+    })
+    .catch(console.log);
 }
 
 function appendHitsMarkup(object) {
-  console.log(object)
-  filmsList.insertAdjacentHTML('beforeend', renderImages(object))
+  console.log(object);
+  filmsList.insertAdjacentHTML('beforeend', renderImages(object));
 }
-
--- src/js/api/fetch-api.js
+++ src/js/api/fetch-api.js
@@ -3,28 +3,28 @@ const API_KEY = `eba0388c934688725105b53c98cf82ca`;
 
 const VIDEO_BY_SEARCH = `${BASE_URL}/search/movie?api_key=${API_KEY}&include_adult=false`;
 
-export default class MovieApiService{
- constructor() {
-  this.searchQuery = '';
-  this.page = 1;
-  this.language = 'ru-US';
-  this.genres = '';
+export default class MovieApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.language = 'ru-US';
+    this.genres = '';
   }
 
   // ======== поиск фильмов ======== //
-   fetchMoviesBySearch() {
-   return fetch(
-    `${VIDEO_BY_SEARCH}&query=${this.searchQuery}&language=${this.language}&page=${this.page}`,
+  fetchMoviesBySearch() {
+    return fetch(
+      `${VIDEO_BY_SEARCH}&query=${this.searchQuery}&language=${this.language}&page=${this.page}`,
     ).then(response => response.json());
-   }
-
- incrementPage() {
-   this.page += 1;
   }
- resetPage() {
+
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
     this.page = 1;
   }
- get query() {
+  get query() {
     return this.searchQuery;
   }
   set query(newQuery) {
