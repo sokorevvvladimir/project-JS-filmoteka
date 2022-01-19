a04e252984fda5962788e2234fe71f5ea3508617
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2022-01-20 00:29:11 +0200

    modal close-esc

--- src/js/components/film-description-modal.js
+++ src/js/components/film-description-modal.js
@@ -5,11 +5,42 @@ import MovieApiService from '../api/fetch-api';
 import { getImgPath } from '../utils/normalizationObj';
 
 const modalDialog = document.querySelector('.modal-one-film');
+const body = document.querySelector('.body');
 const modalContent = document.querySelector('.modal-one-film__content');
 const closeButton = document.querySelector('.modal-close-btn');
 
-closeButton.addEventListener('click', () => {
+function toggleModal() {
     modalDialog.classList.toggle('modal-one-film--hidden');
+    body.classList.toggle('blocked');
+};
+
+function openModal() {
+    modalDialog.classList.remove('modal-one-film--hidden');
+    body.classList.add('blocked');
+}
+
+function closeModal() {
+    modalDialog.classList.add('modal-one-film--hidden');
+    body.classList.remove('blocked');
+}
+
+closeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    closeModal();
+});
+
+modalDialog.addEventListener('click', (e) => {
+    if (e.target !== modalDialog) {
+        return;
+    }
+    closeModal();
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode !== 27) {
+        return;
+    }
+    closeModal();
 });
 
 const movieApiService = new MovieApiService();
@@ -24,22 +55,19 @@ document.querySelector('.films__container').addEventListener('click', event => {
             const id = link.dataset.id;
 
             movieApiService.getMovieDetails(id).then(res => {
-                // normalizationMovieObj(res) не подходит, из АПИ прилетает другая структура:
-                // - жанры сразу с названиями
-                // - два названия фильма (переведённое и оригинальное)
 
                 const data = {// ставим данные в модалку
                     ...res,
                     genre: res.genres.map(g => g.name).join(', '),
                     img: getImgPath(res.poster_path)
                 };
-
                 modalContent.innerHTML = movieModal(data);
-                modalDialog.classList.toggle('modal-one-film--hidden');
+                openModal();
             });
 
             return;
         }
     }
 });
+
 
