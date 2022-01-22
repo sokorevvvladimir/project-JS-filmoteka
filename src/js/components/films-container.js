ee4fd7b00cf8aaffe348b334e5b118b6275f954f
sokorevvvladimir <sokorevvvladimir@gmail.com>
2022-01-22 11:21:56 +0200

    placeholder div

--- src/js/components/films-container.js
+++ src/js/components/films-container.js
@@ -1,12 +1,9 @@
 import { refs } from '../utils/refs';
 
-import filmsListPlaceholder1x from '../../images/films-container-placeholder/container-placeholder-1x.jpg';
-import filmsListPlaceholder2x from '../../images/films-container-placeholder/container-placeholder-2x.jpg';
-
 const placeholderSetter = () => {
-  refs.filmsList.innerHTML = `<img srcset="${filmsListPlaceholder1x} 1x, ${filmsListPlaceholder2x} 2x"
-  src="${filmsListPlaceholder1x}"
-  alt="placeholder" class="img-placeholder"/>`;
+  refs.filmsList.innerHTML = `<div class="div-placeholder">
+  Nothing here so far
+  </div>`;
 };
 
 export { placeholderSetter };
