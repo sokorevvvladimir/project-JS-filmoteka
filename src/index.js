850350155a2f27ffaa75395ed3c4d11875491035
VladimirBludov <vladimir.bloodov@gmail.com>
2022-01-22 17:17:31 +0200

    class pagination created

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import './sass/main.scss';
 
+import './js/components/home';
+import './js/components/search-by-query';
+import './js/components/watched';
+
 import './js/components/togglePages';
 import './js/components/autorization';
-
 import './js/components/film-description-modal';
-
-import './js/components/search-by-query';
-import './js/components/createMoviesList';
-
-import './js/components/modal-footer-friends';
-import './js/components/watched';
-import './js/utils/pagination';
--- src/js/components/watched.js
+++ src/js/components/watched.js
@@ -1,29 +1,27 @@
-import { loadFromLocalStorage } from "../utils/utils"
-import { renderMoviesList } from './createMoviesList.js';
-import { refs } from "../utils/refs"
-const watchedBtn = document.querySelector(".js-library-watched")
+import { loadFromLocalStorage } from '../utils/utils';
+import { renderMoviesList } from '../utils/createMoviesList';
+import { refs } from '../utils/refs';
+const watchedBtn = document.querySelector('.js-library-watched');
 
-function markupAdapter(itemListFromLocalStorage){
-  return itemListFromLocalStorage.map(({ id,genres,poster_path,original_title,release_date,vote_average })=>({
-    id,
-    genre_ids: genres.map((genre)=> genre.id),
-    poster_path,
-    original_title,
-    release_date,
-    vote_average,
-  }))
+function markupAdapter(itemListFromLocalStorage) {
+  return itemListFromLocalStorage.map(
+    ({ id, genres, poster_path, original_title, release_date, vote_average }) => ({
+      id,
+      genre_ids: genres.map(genre => genre.id),
+      poster_path,
+      original_title,
+      release_date,
+      vote_average,
+    }),
+  );
 }
 
-function markup(items){
-  if(!items){
-    return
+function markup(items) {
+  if (!items) {
+    return;
   }
-  refs.filmsList.innerHTML="";
-  renderMoviesList(markupAdapter(items))
+  refs.filmsList.innerHTML = '';
+  renderMoviesList(markupAdapter(items));
 }
 
-watchedBtn.addEventListener("click", () => markup(loadFromLocalStorage("watched")));
-
-
-
-
+watchedBtn.addEventListener('click', () => markup(loadFromLocalStorage('watched')));
