b1b6518c7233e24f6be29eb930b2de034e3436c3
VladimirBludov <vladimir.bloodov@gmail.com>
2022-01-24 09:38:21 +0200

    save

--- src/js/components/trailer.js
+++ src/js/components/trailer.js
@@ -1,7 +1,10 @@
+import YouTubePlayer from 'youtube-player';
 import MovieApiService from '../api/fetch-api.js';
 import { refs } from '../utils/refs.js';
 
 const movieApiService = new MovieApiService();
+
+let player = null;
 
 refs.filmsList.addEventListener('click', onTrailerBtnClick);
 
@@ -14,7 +17,7 @@ async function onTrailerBtnClick(event) {
   const link = cardContainer.querySelector('.movies__link');
   const id = link.dataset.id;
 
-  const trailerId = getTrailer(id);
+  const trailerId = await getTrailer(id);
 
   createPlayer(trailerId);
 
@@ -29,29 +32,31 @@ const closeOnEsc = e => {
 
 function openModal() {
   document.addEventListener('keydown', closeOnEsc);
+  refs.closeButton.addEventListener('click', e => {
+    e.preventDefault();
+    closeModal();
+  });
+
+  refs.trailerBackdrop.addEventListener('click', e => {
+    if (e.target !== refs.trailerBackdrop) {
+      return;
+    }
+    closeModal();
+  });
   refs.trailerBackdrop.classList.remove('trailer__backdrop--hidden');
   refs.html.classList.add('disable-scroll');
 }
 
 function closeModal() {
+  console.log(player);
+  player?.stopVideo();
+  player = null;
   document.removeEventListener('keydown', closeOnEsc);
   refs.trailerBackdrop.classList.add('trailer__backdrop--hidden');
   refs.html.classList.remove('disable-scroll');
 }
 
-refs.closeButton.addEventListener('click', e => {
-  e.preventDefault();
-  closeModal();
-});
-
-refs.trailerBackdrop.addEventListener('click', e => {
-  if (e.target !== refs.trailerBackdrop) {
-    return;
-  }
-  closeModal();
-});
-
-export async function getTrailer(id) {
+async function getTrailer(id) {
   const response = await movieApiService.fetchTrailer(id);
   const videoId = response.results[0].key;
   return videoId;
@@ -59,39 +64,12 @@ export async function getTrailer(id) {
 
 function createPlayer(id) {
   const playerContainer = document.getElementById('player');
-  let player;
-  onYouTubeIframeAPIReady(id);
-  player;
 
-  function onYouTubeIframeAPIReady(id) {
-    player = new YT.Player(playerContainer, {
-      height: '200',
-      width: '100%',
-      videoId: id,
-      host: 'http://www.youtube.com',
-      playerVars: {
-        enablejsapi: 1,
-        autoplay: 0,
-        frameborder: 0,
-        origin: 'http://example.com',
-      },
-      events: {
-        onReady: onPlayerReady,
-        onStateChange: onPlayerStateChange,
-      },
-    });
-  }
+  player = YouTubePlayer(playerContainer, {
+    videoId: id,
+  });
 
-  function onPlayerReady(event) {
-    player.loadVideoById(id);
-    stopVideo();
-  }
+  player.playVideo();
 
-  function onPlayerStateChange(event) {
-    //Body
-  }
-
-  function stopVideo() {
-    player.stopVideo();
-  }
+  return player;
 }
