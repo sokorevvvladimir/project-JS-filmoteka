a5b6c06a7e673565459ee6648aa8a89e73e63271
forysiak-vova <volodja.forus@gmail.com>
2022-01-24 21:09:11 +0100

    салют :)

--- src/js/components/modal-footer-friends.js
+++ src/js/components/modal-footer-friends.js
@@ -1,18 +1,41 @@
+import confetti from 'canvas-confetti';
 
 const open = document.querySelector('.footer__link');
 const close = document.querySelector('.modal__button');
 const modal = document.querySelector('.backdrop');
 
- open.addEventListener('click', toggleModal);
-close.addEventListener('click', toggleModal);
+//  open.addEventListener('click', toggleModal);
+// close.addEventListener('click', toggleModal);
  window.addEventListener('click', onCloseModal)
  
- function toggleModal() {
-     modal.classList.toggle('is-hidden');
+//  function toggleModal() {
+//    modal.classList.toggle('is-hidden');
+//   if ( modal.classList !== 'is-hidden') {
+//  showConfetti()
+// }
+// }
+
+ open.addEventListener('click', onOpenModal);
+close.addEventListener('click', onClosesModal);
+function onOpenModal() {
+  modal.classList.remove('is-hidden');
+  showConfetti()
 }
+
+function onClosesModal() { 
+  modal.classList.add('is-hidden')
+}
+
 function onCloseModal(e) { 
   if (e.target === modal) {
      modal.classList.add('is-hidden');
    }
   
+}
+
+function showConfetti() {
+  confetti.create(document.getElementById('canvas'), {
+    resize: true,
+    useWorker: true,
+  })({ particleCount: 200, spread: 200, zIndex: 200});
 }
